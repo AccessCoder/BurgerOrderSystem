b0fd00518461e17b34de5fd4fcb65dc52fb98cb5
Dominic <andro5@web.de>
2022-03-31 14:34:13 +0200

    exception fixed

--- src/main/java/OrderSystem.java
+++ src/main/java/OrderSystem.java
@@ -1,33 +1,40 @@
 import java.util.HashMap;
-import java.util.InputMismatchException;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class OrderSystem {
 
-    private Map<Integer, Menu> orderSystem = new HashMap<>();
-    private Scanner scanner = new Scanner(System.in);
-    private int amountOfMenus = 0;
+    private Map<String, Menu> orderSystem = new HashMap<>();
 
     public OrderSystem() {
     }
 
     public String addNewMenuToTheMap(Menu menu) {
-        amountOfMenus++;
-        orderSystem.put(amountOfMenus, menu);
+        orderSystem.put(String.valueOf(orderSystem.size() + 1), menu);
         return menu.getMenuName();
     }
 
     public String placeOrder() {
         System.out.println("Please choose a menu between 1 and " + orderSystem.size());
-        int chosenMenu = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
         try {
-            Menu order = orderSystem.get(chosenMenu);
-            System.out.println(order.toString());
-            return order.getMenuName();
-        } catch (NullPointerException | InputMismatchException e) {
-            placeOrder();
+            String chosenMenu = scanner.nextLine();
+            return getOrderById(chosenMenu).getMenuName();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return placeOrder();
         }
-        return "";
+    }
+
+    private Menu getOrderById(String id) {
+        if (orderSystem.containsKey(id)) {
+            Menu order = orderSystem.get(id);
+            System.out.println(order.toString());
+            return order;
+        } else {
+            throw new NoSuchElementException("Menü existiert nicht");
+        }
     }
 }
